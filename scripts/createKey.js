7791bdf02939c8c96deb8427c46ed39fb627e2d6
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-05-09 23:52:05 +0300

    feat: add class Key for create key in keyboard

---
+++ scripts/createKey.js
@@ -0,0 +1,38 @@
+import languages from "./languages.js";
+
+import languages from './lang/languages.js';
+
+const fucctionalKey = ['AltLeft', 'AltRight', 'ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Backspace', 'CapsLock', 'ControlLeft', 'ControlRight', 'Delete', 'Enter', 'ShiftLeft', 'ShiftRight', 'Tab', 'Lang', 'Space'];
+
+function createElement(tag, className, dataName, data, textContent) {
+  const element = document.createElement(tag);
+  if (className) element.classList.add(className);
+  if (data) element.dataset[dataName] = data;
+  if (textContent) element.textContent = textContent;
+
+  return element;
+}
+class Key {
+  constructor(langCode, code) {
+    function findKeyObj() {
+      return languages[langCode].find((element) => element.code === code);
+    }
+
+    this.min = findKeyObj().min;
+    this.shift = findKeyObj().shift;
+    this.code = code;
+
+    if (code === 'Lang') {
+      this.min = langCode;
+      this.shift = langCode;
+    }
+    this.keyHTML = createElement('div', 'key', 'code', this.code, this.min);
+    this.isfucctionalKey = fucctionalKey.includes(code);
+    
+    if (this.isfucctionalKey) {
+      this.keyHTML.classList.add(this.code);
+      this.keyHTML.dataset.isfucctionalKey = 'true';
+    }
+  }
+}
+export default Key;
